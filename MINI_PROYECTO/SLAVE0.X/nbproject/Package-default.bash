efbce1e267dbfa588b98d60e7e9b7940e23e3b40
Kenneth Aldana <ald18435@uvg.edu.gt>
2021-02-23 16:57:25 -0600

    Mini proyecto
    Archivos y básico de programas de maestro y 3 esclavos.

--- MINI_PROYECTO/SLAVE0.X/nbproject/Package-default.bash
+++ MINI_PROYECTO/SLAVE0.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/MINI_PROYECTO.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=MINI_PROYECTO.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=miniproyecto/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/SLAVE0.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=SLAVE0.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=slave0.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/miniproyecto/bin
+makeDirectory ${TMPDIR}/slave0.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/miniproyecto.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/slave0.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/miniproyecto.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/slave0.x.tar *
 checkReturnCode
 
 # Cleanup
