2ebaa66509ad6a7c4d3b0b6f91c48e620dbfded2
Kenneth Aldana <ald18435@uvg.edu.gt>
2021-03-29 22:42:46 -0600

    Declaracion de pines para progra
    LAB04

---
+++ LAB04/LAB04/LAB04.ino
@@ -0,0 +1,26 @@
+//PINES PARA EL SEMAFORO
+int ledR = RED_LED;
+int ledG = GREEN_LED;
+int ledB = BLUE_LED;
+
+// PINES PARA LOS JUGADORES, 1-8 JUGADOR 1, 9-16 JUGADOR 2
+int led1 = PB_5;
+int led2 = PB_0;
+int led3 = PB_1;
+int led4 = PE_4;
+int led5 = PE_5;
+int led6 = PB_4;
+int led7 = PA_5;
+int led8 = PA_6;
+int led9 = PD_0;
+int led10 = PD_1;
+int led11 = PD_2;
+int led12 = PD_3;
+int led13 = PE_1;
+int led14 = PE_2;
+int led15 = PE_3;
+int led16 = PF_1;
+
+//LED PARA GANADORES
+int led17 = PC_4;
+int led18 = PC_5;
