d5318bc4eb14cbe925323947eec2ba42f20d18d7
Kenneth Aldana <ald18435@uvg.edu.gt>
2021-05-11 17:35:32 -0600

    LAB06
    Se agrega el PDF con las respuestas de la parte 02 y 03, también se agrega los comentarios de la programación.

--- LAB06/main.c
+++ LAB06/main.c
@@ -3,6 +3,9 @@
 //SECCION
 //LABORATORIO 06
 //ELECTRONICA DIGITAL 02
+
+
+//Incluyo las librerias y todo lo que necesitare para poder trabajar
 #include <stdint.h>
 #include <stdbool.h>
 #include "driverlib/gpio.c"
@@ -18,34 +21,31 @@
 
 int main(void)
 {
+    //Configuracion del reloj  para una frecuencia de 40MHz y con el oscilador principal
     SysCtlClockSet ( SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ );
+    //Se configura para poder utilizar los perifericos del puerto F
     SysCtlPeripheralEnable ( SYSCTL_PERIPH_GPIOF );
 
-    //PARTE02:
-    //Timers:GPTMCTL, GPTMCFG, GPTMTnMR, GPTMTnILR, GPTMIMR, GPTMRIS,GPTMICR
-    //UART: RCGCUART, RCGCGPIO, GPIOPCTL, UARTIBRD, UARTFBRD, UARTLCRH
-    //USB: RCGCUSB, RCGCGPIO, GPIOPCTL, USBGPCS
-    //PWM: RCGC0, RCGC2, GPIOAFSEL, GPIOPCTL, PWM0CTL, PWM0GENA, PWM0GENB, PWM0LOAD, PWM0CMPA,PWM0CMPB, PWM0CTL,PWMENABLE
-    //ADC: RCGCADC, RCGCGPIO, GPIODEN, GPIOAMSEL, ADCSSPRI
-
+    //Se configuran los pines de los leds rojo, azul y verde para que sean salidas
     GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE,GPIO_PIN_1);
     GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE, GPIO_PIN_2);
     GPIOPinTypeGPIOOutput (GPIO_PORTF_BASE, GPIO_PIN_3);
 
-    // PARTE03 :
-    // SysCtlDelay, con parametro de duracion del delay en nanosegundos
-
+    //Se configura el boton uno para que funcione como entrada y weak pull up
     GPIOPinTypeGPIOInput(GPIO_PORTF_BASE , GPIO_PIN_4 );
     GPIOPadConfigSet(GPIO_PORTF_BASE,GPIO_PIN_4, GPIO_STRENGTH_4MA , GPIO_PIN_TYPE_STD_WPU );
 
     while(1) {
+
+        //Se trabaja con hexadecimales para que encienda un respectivo color
         if ((GPIOPinRead (GPIO_PORTF_BASE,GPIO_PIN_4) & 0x16)==0) {
             while ((GPIOPinRead (GPIO_PORTF_BASE,GPIO_PIN_4) & 0x16)==0) { }
-
+            //Se enciende el verde
             GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x08);
             SysCtlDelay (20000000);
             GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x00);
             SysCtlDelay (5000000);
+            //Se enciende el verde que parpadea
             GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x08);
             SysCtlDelay (5000000);
             GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x00);
@@ -58,10 +58,12 @@ int main(void)
             SysCtlDelay (5000000);
             GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x00);
             SysCtlDelay (5000000);
+            //Se enciende el amarillo
             GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x0A);
             SysCtlDelay (20000000);
             GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x00);
             SysCtlDelay (5000000);
+            //Se enciende el rojo
             GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x02);
             SysCtlDelay (20000000);
             GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x00);
